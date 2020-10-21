92a68554c55f576bfb78a4bb7730359dc54af631
Thomas Mullaney <tloring.mullaney@gmail.com>
2020-10-21 13:04:39 -0700

    debugging statistics tracker

--- models/workoutmodel.js
+++ models/workoutmodel.js
@@ -1,72 +1,64 @@
-
 const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const workoutSchema = new Schema({
+const workoutSchema = new Schema(
+  {
+    day: {
+      type: Date,
+      default: Date.now,
+    },
 
-  day: {
-    type: Date,
-    default: Date.now,
+    exercises: [
+      {
+        type: {
+          type: String,
+          trim: true,
+          required: true,
+        },
+
+        name: {
+          type: String,
+          trim: true,
+          required: true,
+        },
+
+        weight: {
+          type: Number,
+        },
+
+        sets: {
+          type: Number,
+        },
+
+        reps: {
+          type: Number,
+        },
+
+        duration: {
+          type: Number,
+          required: true,
+        },
+
+        distance: {
+          type: Number,
+        },
+      },
+    ],
   },
 
-  exercises: [{
-    type: {
-      type: String,
-      trim: true,
-      required: true
-    },
-
-    name: {
-      type: String,
-      trim: true,
-      required: true
-    },
-
-
-    weight: {
-      type: Number,
-      
-    },
-
-    sets: {
-      type: Number,
-      
-    },
-
-
-    reps: {
-      type: Number,
-      
-    },
-
-    duration: {
-      type: Number,
-      required: true
-    },
-
-    distance: {
-      type: Number,
-      
-    }
-
-  }],
-},
-
   {
-    toJSON:
-    {
-      virtuals: true
-    }
+    toJSON: {
+      virtuals: true,
+    },
   }
 );
 
-
 workoutSchema.virtual("totalDuration").get(function () {
-  return this.exercises.reduce((total, exercises) => {
-    return total + exercises.duration;
+  return this.exercises.reduce((total, exercise) => {
+    return total + exercise.duration;
   }, 0);
 });
-const Workout = mongoose.model("workouts", workoutSchema);
+const Workout = mongoose.model("Workout", workoutSchema);
 
 module.exports = Workout;
